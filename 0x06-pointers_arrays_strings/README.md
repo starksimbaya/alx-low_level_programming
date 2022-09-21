715770cad7bdf0900c566cd64dd27c83986f372f
starksimbaya <simbayas@gmail.com>
2022-09-21 08:02:06 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,9 @@
+C - more pointers, arrays and strings
+0 - write a function that concatenates two strings
+1 - write a function that copies a string
+2 - write a fucntion that compares two strings
+3 - write a function that reverses the content of an array of integers
+4 - write a function that changes all lower case of a string to uppercase
+5 - write a function that capitalises all words of a string.
+6 - write a function that encode a string into 1337
+7 - write a fucntion that ecodes a string into rot13
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,24 @@
+#ifdef MAIN_H
+#define MAIN_H
+#include<studio.h>
+/**
+*void prototypes()
+*int prototypes()
+*char prototypes()
+*/
+void reverse_array(int *a, int n);
+void print_number(int n);
+void print_buffer(char *b, int size);
+int_strcmp(char *s1, char *s2);
+char_*strcat(char *dest, char *src)
+char_*strncat(char *dest, char *src, int n);
+char_*strncpy(char *dest, char *src, int n);
+char_*string_toupper(char*)
+char_cap_string(char*);
+
+char *leet(char*);
+char *rot13(char*);
+char *infinite_add(char *n1,char *n2,char *r, int size_r);
+char *add_strings(char *n1, char *n2,char *r, int r_index);
+
+#endif/*MAIN.H*/
